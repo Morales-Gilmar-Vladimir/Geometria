675d1ee9b8f62241ef492bc30ee92c777d1373b3
Gilmar_Morales <gilmarmoralesriva2003@gmail.com>
2023-06-06 07:59:51 -0500

    registrar datos en las dos clases

--- src/figuras2D.java
+++ src/figuras2D.java
@@ -1,2 +1,50 @@
 public class figuras2D {
+    int nLados; //numero de lados
+    float lado; //longitud del lado
+    //constructores
+    public figuras2D(int nLados, float lado) {
+        this.nLados = nLados;
+        this.lado = lado;
+    }
+
+    public figuras2D(float lado) {
+        this.lado = lado;
+    }
+
+    public figuras2D(int nLados) {
+        this.nLados = nLados;
+    }
+
+    //setters y getters
+    public int getnLados() {
+        return nLados;
+    }
+
+    public void setnLados(int nLados) {
+        this.nLados = nLados;
+    }
+
+    public float getLado() {
+        return lado;
+    }
+
+    public void setLado(float lado) {
+        this.lado = lado;
+    }
+
+    public float areaCuadrado(int lado){
+       return this.lado*this.lado;
+    };
+
+    public float perimetroCuadrado(){
+    return 0;
+    };
+
+    public float areaTriangulo(){
+        return 0;
+    };
+
+    public float perimetroTriangulo(){
+        return 0;
+    };
 }
---
+++ src/menu.java
@@ -0,0 +1,9 @@
+public class menu {
+    public static void main (String[] args){
+        figuras2D miFigura=new figuras2D(4, 2);
+
+        miFigura.setLado(3);
+
+        System.out.println(miFigura.areaCuadrado(7));
+    }
+}
